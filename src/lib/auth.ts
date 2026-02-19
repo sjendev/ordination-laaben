41078b0aaaec426c089e1cf7831fbf7a76c6a7c5
sjenwiid <sjenwiid@users.noreply.github.com>
2026-02-19 11:04:43 +0000

    chore: minor updates to auth logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 // Add up to 5 email addresses that are allowed to access the admin dashboard
 const ALLOWED_EMAILS = [
   "sjen@humanadvisory.com",
+  "mogachago@gmail.com",
 ];
 
 export const authOptions: NextAuthOptions = {
