d865f00e159b90a0b5d7393f9c8d8eef5b064e08
sjenwiid <sjenwiid@users.noreply.github.com>
2026-03-15 13:23:41 +0000

    update booking link

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,1 +1,1 @@
-export const BOOKING_LINK = "https://patient.latido.at/booking/select;profileid=5efda4b43d1d20f9ab52161b;coredataid=5efda4b0f4997a87810e83af";
+export const BOOKING_LINK = "https://patient.latido.at/booking/select;profileid=5efda4b43d1d20f9ab52161b;coredataid=698c92c4b082366c6221d5a8";
